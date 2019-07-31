aa433260a602f666e7297759340c3d1f7c61dfff
Valdasmo <51168355+Valdasmo@users.noreply.github.com>
2019-07-31 17:24:14 +0300

    1

--- src/T1.java
+++ src/T1.java
@@ -1,8 +1,6 @@
 public class T1 {
     public static void main(String[] args) {
         System.out.println();
-        System.out.println("Laba diena");
-        System.out.println("Dirbam");
-        System.out.println("Testuojam");
+
     }
 }
