febd07cac962807681342ec1fbd664b833738a7a
Zilvinas <z.lasauskas@gmail.com>
2019-07-31 16:37:50 +0300

    Initial commit

---
+++ src/Print_file.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Print_file {
+
+    public static void main(String args[])throws IOException {
+        File file = new File("Hello1.txt");
+
+        // creates the file
+        file.createNewFile();
+
+        // creates a FileWriter Object
+        FileWriter writer = new FileWriter(file);
+
+        // Writes the content to the file
+        writer.write("This\n is\n an\n example\n");
+        writer.flush();
+        writer.close();
+
+        // Creates a FileReader Object
+        FileReader fr = new FileReader(file);
+        char [] a = new char[50];
+        fr.read(a);   // reads the content to the array
+
+        for(char c : a)
+            System.out.print(c);   // prints the characters one by one
+        fr.close();
+    }
+}
