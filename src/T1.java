cc889acf25808e1bb496cad9ae83008669a7dabb
Valdasmo <51168355+Valdasmo@users.noreply.github.com>
2019-07-31 17:07:40 +0300

    Dirbam!

--- src/T1.java
+++ src/T1.java
@@ -2,5 +2,6 @@ public class T1 {
     public static void main(String[] args) {
         System.out.println();
         System.out.println("Laba diena");
+        System.out.println("Dirbam");
     }
 }
