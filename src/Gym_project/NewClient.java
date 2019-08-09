71153ecb3da9a051f5f69b0eb7e0f87ca82c975b
ZilvinasLasauskas <51168294+ZilvinasLasauskas@users.noreply.github.com>
2019-08-09 17:39:17 +0300

    Update NewClient.java

--- src/Gym_project/NewClient.java
+++ src/Gym_project/NewClient.java
@@ -7,36 +7,36 @@ import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.util.Scanner;
 
-public class NewClient extends Client {
+public class NewClient {
 
-    NewClient() {
-        super();
-    }
+    Client client = new Client();
+    double BMI;
 
     void NewClientCaller() {
         Scanner scanner = new Scanner(System.in);
         LocalDate localDate = LocalDate.now();
-        System.out.println("Enter your id: ");
-        this.setId(scanner.nextLine());
-        System.out.println("enter time spent in minutes: ");
-        this.setTimeSpent(scanner.nextInt());
-        System.out.println("enter height in cm: ");
-        this.setHeight(scanner.nextDouble());
-        System.out.println("enter weight: ");
-        this.setWeight(scanner.nextDouble());
-        this.setBMI((this.getWeight())/((this.getHeight()*this.getHeight())/10000));
-        System.out.println("BMI: " + getBMI());
-        File file = new File (getId() + ".csv");
-        try{
-            if (!file.exists()){
+        System.out.println("Įrašykite kliento id: ");
+        client.setId(scanner.nextLine());
+        System.out.println("Įrašykite kiek laiko sportavote, min: ");
+        client.setTimeSpent(scanner.nextInt());
+        System.out.println("Įrašykite ūgį, cm: ");
+        client.setHeight(scanner.nextDouble());
+        System.out.println("Įrašykite svorį, kg: ");
+        client.setWeight(scanner.nextDouble());
+
+        BMI = Counters.bodyMassIndexCounter(client.height, client.weight);
+
+        File file = new File(client.getId() + ".txt");
+        try {
+            if (!file.exists()) {
                 file.createNewFile();
             }
             PrintWriter out = new PrintWriter(new FileWriter(file, true));
-            out.append(localDate + ": " + "ID: " + getId() + " | Time: " +
-                    this.getTimeSpent() + " | Height: " + this.getHeight() +
-                    " | Weight: " + this.getWeight() + " | BMI: " + this.getBMI() + System.lineSeparator());
+            out.append(localDate + ": " + "ID: " + client.getId() + " | Time: " +
+                    client.getTimeSpent() + " | Height: " + client.getHeight() +
+                    " | Weight: " + client.getWeight() + " | BMI: " + BMI + System.lineSeparator());
             out.close();
-        } catch (IOException ex){
+        } catch (IOException ex) {
             System.out.println(ex);
         }
     }
