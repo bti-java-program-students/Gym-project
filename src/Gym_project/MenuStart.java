71b3949506ca78157ec988a968860331af2651f4
ZilvinasLasauskas <51168294+ZilvinasLasauskas@users.noreply.github.com>
2019-08-12 16:25:13 +0300

    Update MenuStart.java

--- src/Gym_project/MenuStart.java
+++ src/Gym_project/MenuStart.java
@@ -2,32 +2,28 @@ package Gym_project;
 
 import java.util.Scanner;
 
-public class MenuStart {
+class MenuStart {
     static void startProgram() {
         try (Scanner input = new Scanner(System.in)) {
-
-            String Menu = "";
             System.out.println("Labas! Ir vėl nauja puiki diena!");
             System.out.println("Esamas klientas - spauskite 1");
             System.out.println("Naujas klientas - spauskite 2");
             System.out.println("Baigti programos darbą - spauskite 3");
-            Menu = input.nextLine();
+            int Menu = input.nextInt();
             switch (Menu) {
-                case "1":
+                case 1:
                     System.out.println("Pasirinkta 1 - Esamas klientas");
-                    new NewClient().NewClientCaller();
-                    startProgram();
+                    NewClient.FirstTimeInGym();
                     break;
-                case "2":
+                case 2:
                     System.out.println("Pasirinkta 2 - Naujas klientas");
                     FirstInput.newInput();
                     break;
-                case "3":
+                case 3:
                     System.out.println("Pasirinkta 3 - Programa baigė darbą");
                     break;
                 default:
                     System.out.println("Neteisingas pasirinkimas. Bandykite dar karta =))");
-                    startProgram();
             }
         } catch (Exception ex) {
             ex.printStackTrace();
