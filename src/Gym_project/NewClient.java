59281c19432af8b6046b2d21d20824254189f2a2
ZilvinasLasauskas <51168294+ZilvinasLasauskas@users.noreply.github.com>
2019-08-12 16:25:56 +0300

    Update NewClient.java

--- src/Gym_project/NewClient.java
+++ src/Gym_project/NewClient.java
@@ -1,43 +1,60 @@
 package Gym_project;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.util.Scanner;
 
-public class NewClient {
+import static Gym_project.MenuStart.startProgram;
 
-    Client client = new Client();
-    double BMI;
+class NewClient {
 
-    void NewClientCaller() {
-        Scanner scanner = new Scanner(System.in);
-        LocalDate localDate = LocalDate.now();
-        System.out.println("Įrašykite kliento id: ");
-        client.setId(scanner.nextLine());
-        System.out.println("Įrašykite kiek laiko sportavote, min: ");
-        client.setTimeSpent(scanner.nextInt());
-        System.out.println("Įrašykite ūgį, cm: ");
-        client.setHeight(scanner.nextDouble());
-        System.out.println("Įrašykite svorį, kg: ");
-        client.setWeight(scanner.nextDouble());
+    static Client client = new Client();
 
-        BMI = Counters.bodyMassIndexCounter(client.height, client.weight);
+    static void FirstTimeInGym() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            LocalDate localDate = LocalDate.now();
+            System.out.println("Įrašykite kliento id: ");
+            client.setId(scanner.nextLine());
+            String path = client.getId() + ".csv";
+            System.out.println("Įrašykite kiek laiko sportavote, min: ");
+            client.setTimeSpent(scanner.nextInt());
+            System.out.println("Įrašykite ūgį, cm: ");
+            client.setHeight(scanner.nextDouble());
+            System.out.println("Įrašykite svorį, kg: ");
+            client.setWeight(scanner.nextDouble());
+            client.setBMI(Counters.bodyMassIndexCounter(client.getHeight(), client.getWeight()));
 
-        File file = new File(client.getId() + ".txt");
-        try {
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-            PrintWriter out = new PrintWriter(new FileWriter(file, true));
-            out.append(localDate + ": " + "ID: " + client.getId() + " | Time: " +
-                    client.getTimeSpent() + " | Height: " + client.getHeight() +
-                    " | Weight: " + client.getWeight() + " | BMI: " + BMI + System.lineSeparator());
-            out.close();
-        } catch (IOException ex) {
-            System.out.println(ex);
+            StringBuilder head = new StringBuilder();
+            head.append("date");
+            head.append(',');
+            head.append("id");
+            head.append(',');
+            head.append("height");
+            head.append(',');
+            head.append("weight");
+            head.append(',');
+            head.append("BMI");
+            head.append(',');
+            head.append("TimeSpent");
+
+            StringBuilder clientInfo = new StringBuilder();
+            clientInfo.append(localDate);
+            clientInfo.append(',');
+            clientInfo.append(client.getId());
+            clientInfo.append(',');
+            clientInfo.append(client.getHeight());
+            clientInfo.append(',');
+            clientInfo.append(client.getWeight());
+            clientInfo.append(',');
+            clientInfo.append(client.getBMI());
+            clientInfo.append(',');
+            clientInfo.append(client.getTimeSpent());
+
+            Writer.WriteInfoToCSV(head, clientInfo, path);
+            startProgram();
+        }  catch (Exception ex){
+            System.out.println("That's your problem---->" + ex);
         }
+
     }
 }
